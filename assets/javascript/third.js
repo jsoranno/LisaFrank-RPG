6e56100c7ce9b18121a395c9f4bc031b6211d980
Juanita Soranno <juanitasoranno@gmail.com>
2016-08-31 22:36:02 -0400

    Changed .js link to Third.js and changed var

--- assets/javascript/third.js
+++ assets/javascript/third.js
@@ -1,11 +1,9 @@
 // VARIABLES
 // ==========================================================================
-var allHouses = {
-		houseStark: {name: "Stark", hp: 500, ap: 35, cap: 50},
-		houseBar: {name: "Baratheon", hp: 450, ap: 70, cap: 25},
-		houseTar: {name: "Targaryan", hp: 475, ap: 50, cap: 15},
-		houseBar2: {name: "Baratheonandon", hp: 470, ap:40, cap:40}
-	};
+var	houseStark = {name: "Stark", hp: 500, ap: 35, cap: 50};
+var houseBar = {name: "Baratheon", hp: 450, ap: 70, cap: 25};
+var houseTar = {name: "Targaryan", hp: 475, ap: 50, cap: 15};
+var houseBar2 = {name: "Baratheonandon", hp: 470, ap:40, cap:40};
 var house = " ";
 var houseHP = 0;
 var	houseAP = 0;

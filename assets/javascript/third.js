26b61b900a17d5f2783332287f5a9ed54e6c489e
Juanita Soranno <juanitasoranno@gmail.com>
2016-08-31 22:22:23 -0400

    Added fourth player

---
+++ assets/javascript/third.js
@@ -0,0 +1,21 @@
+// VARIABLES
+// ==========================================================================
+var allHouses = {
+		houseStark: {name: "Stark", hp: 500, ap: 35, cap: 50},
+		houseBar: {name: "Baratheon", hp: 450, ap: 70, cap: 25},
+		houseTar: {name: "Targaryan", hp: 475, ap: 50, cap: 15},
+		houseBar2: {name: "Baratheonandon", hp: 470, ap:40, cap:40}
+	};
+var house = " ";
+var houseHP = 0;
+var	houseAP = 0;
+var counterHP = 0;
+var	counterAP: 0;
+var	mainHouse: false;
+var	vsHouse: false;
+var	vsHouseLeft: true;
+
+// FUNCTION DECLARATIONS
+// ==============================================================================	 
+
+chooseHouse
